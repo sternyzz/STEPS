c82955176f600ab4b50761a1a00c6d978b000ec7
sternyzz <sa1344@srmist.edu.in>
2026-02-23 00:51:50 +0530

    Update OOPSBANNER.java

--- OOPSBANNER.java
+++ OOPSBANNER.java
@@ -1,18 +1,27 @@
 /**
- * UC3 - Full OOPS Banner using String.join(" ", ...)
- 
+ * UC4 - Full OOPS Banner
+ * 1. Define a String array with size equal to number of lines in the banner
+ * 2. Populate each index with the corresponding banner line using String.join()
+ * 3. Use a for-each loop to iterate through the array and print each line
  */
-public class stepoops{
+public class uc4banner {
     public static void main(String[] args) {
 
-        // Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
-        System.out.println(String.join("   ", "   ***  ", "   ***  ", "  *****  ", "  ***** "));
-        System.out.println(String.join("   ", " **   **", " **   **", " **   **", " **     "));
-        System.out.println(String.join("   ", "**     **", "**     **", "**   **", "**      "));
-        System.out.println(String.join("   ", "**     **", "**     **", "*****   ", " *****  "));
-        System.out.println(String.join("   ", "**     **", "**     **", "**      ", "      **"));
-        System.out.println(String.join("   ", " **   ** ", " **   ** ", "**      ", " **   **"));
-        System.out.println(String.join("   ", "  ***    ", "  ***    ", "**      ", "   ***   "));
+        // Define a String array with size equal to number of lines in the banner
+        String[] lines = new String[7];
 
+        // Populate each index of the array with the corresponding banner line using String.join()
+        lines[0] = String.join("   ", "   ***  ", "   ***  ",   "  *****  ", "  ******");
+        lines[1] = String.join("   ", " **   **", " **   **",   "  **   **", " **    ");
+        lines[2] = String.join("   ", "**     **", "**     **", "**    **", "**     ");
+        lines[3] = String.join("   ", "**     **", "**     **", "*****   ", " ***** ");
+        lines[4] = String.join("   ", "**     **", "**     **", "**      ", "      **");
+        lines[5] = String.join("   ", " **   ** ", " **   ** ", "**      ", " **   **");
+        lines[6] = String.join("   ", "  ***    ", "  ***    ", "**      ", "  ***  ");
+
+        // Use a for-each loop to iterate through the array and print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
